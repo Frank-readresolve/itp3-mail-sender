a6a72e9e0459a22cd54ca4fb9aa735d0c77a0725
DamlaSub <mme.dalloway@gmail.com>
2023-07-25 10:33:44 +0200

    first commit

---
+++ itp3-mail-sender-api/src/main/java/co/simplon/itp3/mailsender/Itp3MailSenderApiApplication.java
@@ -0,0 +1,13 @@
+package co.simplon.itp3.mailsender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Itp3MailSenderApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Itp3MailSenderApiApplication.class, args);
+	}
+
+}
---
+++ itp3-mail-sender-api/src/test/java/co/simplon/itp3/mailsender/Itp3MailSenderApiApplicationTests.java
@@ -0,0 +1,13 @@
+package co.simplon.itp3.mailsender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Itp3MailSenderApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
