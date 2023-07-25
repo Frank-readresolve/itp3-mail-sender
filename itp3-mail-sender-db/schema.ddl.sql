85eed3d914b8a52c18365e755fb5ee6645792adb
Raulet Olha <raulet.olha@gmail.com>
2023-07-25 16:54:38 +0200

    DB: modify size of contact_email

--- itp3-mail-sender-db/schema.ddl.sql
+++ itp3-mail-sender-db/schema.ddl.sql
@@ -6,7 +6,7 @@ CREATE TABLE customers (
     customer_name VARCHAR(100) UNIQUE NOT NULL,
     first_name VARCHAR(100) NOT NULL,
     last_name VARCHAR(100) NOT NULL,
-    contact_email VARCHAR(255) UNIQUE NOT NULL,
+    contact_email VARCHAR(256) UNIQUE NOT NULL,
     marketing_consent BOOLEAN NOT NULL
 );
 
