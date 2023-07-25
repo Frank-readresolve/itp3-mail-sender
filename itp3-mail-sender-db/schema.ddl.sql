09f21026c5131444bda718ab798977409d7b7263
Raulet Olha <raulet.olha@gmail.com>
2023-07-25 12:44:41 +0200

    Add schema ddl

---
+++ itp3-mail-sender-db/schema.ddl.sql
@@ -0,0 +1,13 @@
+DROP TABLE IF EXISTS customers;
+
+CREATE TABLE customers (
+    id SERIAL PRIMARY KEY,
+    customer_number SERIAL UNIQUE,
+    customer_name VARCHAR(100) UNIQUE NOT NULL,
+    first_name VARCHAR(100) NOT NULL,
+    last_name VARCHAR(100) NOT NULL,
+    contact_email VARCHAR(255) UNIQUE NOT NULL,
+    marketing_consent BOOLEAN NOT NULL
+);
+
+ALTER SEQUENCE customers_customer_number_seq RESTART WITH 10000;
