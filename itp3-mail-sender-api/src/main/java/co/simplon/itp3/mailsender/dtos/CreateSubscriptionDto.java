2bca6dc92ac8c2deaa8b3cbc5349b176d192d388
DamlaSub <mme.dalloway@gmail.com>
2023-07-26 12:34:11 +0200

    add subscription entity & dto

---
+++ itp3-mail-sender-api/src/main/java/co/simplon/itp3/mailsender/dtos/CreateSubscriptionDto.java
@@ -0,0 +1,80 @@
+package co.simplon.itp3.mailsender.dtos;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+public class CreateSubscriptionDto {
+
+    @NotBlank
+    @Size(max = 100)
+    private String subscriptionName;
+    @NotBlank
+    @Pattern(regexp = "[a-zA-Z0-9]+")
+    private String description;
+    @NotNull
+    private Boolean freeSubscription;
+    @Min(1)
+    @Max(12)
+    private Short durationInMonths;
+    @NotNull
+    @Min(11)
+    @Max(1000000)
+    private int maxMailNum;
+
+    public String getSubscriptionName() {
+	return subscriptionName;
+    }
+
+    public void setSubscriptionName(
+	    String subscriptionName) {
+	this.subscriptionName = subscriptionName;
+    }
+
+    public String getDescription() {
+	return description;
+    }
+
+    public void setDescription(String description) {
+	this.description = description;
+    }
+
+    public Boolean getFreeSubscription() {
+	return freeSubscription;
+    }
+
+    public void setFreeSubscription(
+	    Boolean freeSubscription) {
+	this.freeSubscription = freeSubscription;
+    }
+
+    public Short getDurationInMonths() {
+	return durationInMonths;
+    }
+
+    public void setDurationInMonths(
+	    Short durationInMonths) {
+	this.durationInMonths = durationInMonths;
+    }
+
+    public int getMaxMailNum() {
+	return maxMailNum;
+    }
+
+    public void setMaxMailNum(int maxMailNum) {
+	this.maxMailNum = maxMailNum;
+    }
+
+    @Override
+    public String toString() {
+	return "{subscriptionName=" + subscriptionName
+		+ ", description=" + description
+		+ ", freeSubscription=" + freeSubscription
+		+ ", durationInMonths=" + durationInMonths
+		+ ", maxMailNum=" + maxMailNum + "}";
+    }
+
+}
---
+++ itp3-mail-sender-api/src/main/java/co/simplon/itp3/mailsender/entities/Subscription.java
@@ -0,0 +1,89 @@
+package co.simplon.itp3.mailsender.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "subscriptions")
+public class Subscription {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "subscription_name")
+    private String subscriptionName;
+    @Column(name = "description")
+    private String description;
+    @Column(name = "free_subscription")
+    private Boolean freeSubscription;
+    @Column(name = "duration_in_months")
+    private Short durationInMonths;
+    @Column(name = "max_mail_num")
+    private int maxMailNum;
+
+    public Long getId() {
+	return id;
+    }
+
+    public void setId(Long id) {
+	this.id = id;
+    }
+
+    public String getSubscriptionName() {
+	return subscriptionName;
+    }
+
+    public void setSubscriptionName(
+	    String subscriptionName) {
+	this.subscriptionName = subscriptionName;
+    }
+
+    public String getDescription() {
+	return description;
+    }
+
+    public void setDescription(String description) {
+	this.description = description;
+    }
+
+    public Boolean getFreeSubscription() {
+	return freeSubscription;
+    }
+
+    public void setFreeSubscription(
+	    Boolean freeSubscription) {
+	this.freeSubscription = freeSubscription;
+    }
+
+    public Short getDurationInMonths() {
+	return durationInMonths;
+    }
+
+    public void setDurationInMonths(
+	    Short durationInMonths) {
+	this.durationInMonths = durationInMonths;
+    }
+
+    public int getMaxMailNum() {
+	return maxMailNum;
+    }
+
+    public void setMaxMailNum(int maxMailNum) {
+	this.maxMailNum = maxMailNum;
+    }
+
+    @Override
+    public String toString() {
+	return "{id=" + id + ", subscriptionName="
+		+ subscriptionName + ", description="
+		+ description + ", freeSubscription="
+		+ freeSubscription + ", durationInMonths="
+		+ durationInMonths + ", maxMailNum="
+		+ maxMailNum + "}";
+    }
+
+}
