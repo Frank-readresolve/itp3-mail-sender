065ce9242b7b5a424b541791f60bb8555231dd6c
DamlaSub <mme.dalloway@gmail.com>
2023-07-26 11:36:28 +0200

    add customers table

--- itp3-mail-sender-db/schema.ddl.sql
+++ itp3-mail-sender-db/schema.ddl.sql
@@ -1,4 +1,5 @@
 DROP TABLE IF EXISTS customers;
+DROP TABLE IF EXISTS subscriptions;
 
 CREATE TABLE customers (
     id SERIAL PRIMARY KEY,
@@ -8,6 +9,15 @@ CREATE TABLE customers (
     last_name VARCHAR(100) NOT NULL,
     contact_email VARCHAR(256) UNIQUE NOT NULL,
     marketing_consent BOOLEAN NOT NULL
+);
+
+CREATE TABLE subscriptions(
+	id SERIAL PRIMARY KEY,
+	subscription_name VARCHAR(100) UNIQUE NOT NULL,
+	description VARCHAR(1000) NOT NULL,
+	free_subscription BOOLEAN,
+	duration_in_months SMALLINT NOT NULL,
+	max_mail_num INTEGER NOT NULL
 );
 
 ALTER SEQUENCE customers_customer_number_seq RESTART WITH 10000;
