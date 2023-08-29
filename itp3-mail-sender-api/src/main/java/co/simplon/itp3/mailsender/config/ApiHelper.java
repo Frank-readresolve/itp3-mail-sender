5fe5c9a8b89f2200e2a113bd98287cf5ab68a0db
JulietteMadi <juliette.madinier@gmail.com>
2023-08-29 15:20:39 +0200

    #43: ApiKey dried

--- itp3-mail-sender-api/src/main/java/co/simplon/itp3/mailsender/config/ApiHelper.java
+++ itp3-mail-sender-api/src/main/java/co/simplon/itp3/mailsender/config/ApiHelper.java
@@ -1,37 +1,18 @@
 package co.simplon.itp3.mailsender.config;
 
-import java.time.Instant;
-
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 
 public class ApiHelper {
-    private final String issuer;
-    private final long expiration;
-    private final Algorithm algorithm;
     private final PasswordEncoder encoder;
 
     public ApiHelper(Builder builder) {
-	this.issuer = builder.issuer;
-	this.algorithm = builder.algorithm;
 	this.encoder = builder.encoder;
-	this.expiration = builder.expiration;
     }
 
     public String encode(String apiKey) {
 	return encoder.encode(apiKey);
-    }
-
-    public String createJWT(String name) {
-	Instant now = Instant.now();
-	Instant expirationTime = now
-		.plusSeconds(expiration);
-	return JWT.create().withIssuer(issuer)
-		.withSubject(name).withIssuedAt(now)
-		.withExpiresAt(expirationTime)
-		.sign(algorithm);
     }
 
     public static class Builder {
--- itp3-mail-sender-api/src/main/java/co/simplon/itp3/mailsender/config/SecurityConfig.java
+++ itp3-mail-sender-api/src/main/java/co/simplon/itp3/mailsender/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package co.simplon.itp3.mailsender.config;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -19,18 +20,14 @@ public class SecurityConfig {
     @Value("${mailsender.auth.secret}")
     private String secret;
 
-    @Value("${mailsender.auth.tokenExpiration}")
-    private long tokenExpiration;
-
+    @Bean
     public ApiHelper apiHelper() {
 	Algorithm algorithm = Algorithm.HMAC256(secret);
 	PasswordEncoder encoder = new BCryptPasswordEncoder(
 		rounds);
 
 	return new ApiHelper.Builder().algorithm(algorithm)
-		.encoder(encoder)
-		.expiration(tokenExpiration).issuer(issuer)
-		.build();
+		.encoder(encoder).issuer(issuer).build();
     }
 
 }
