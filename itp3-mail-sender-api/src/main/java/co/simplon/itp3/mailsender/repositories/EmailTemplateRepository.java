a4476bb468153e14c98440be8c0cfc78a5161573
JulietteMadi <juliette.madinier@gmail.com>
2023-08-30 14:47:52 +0200

    ApiKey Mail sent when account created

--- itp3-mail-sender-api/src/main/java/co/simplon/itp3/mailsender/repositories/EmailTemplateRepository.java
+++ itp3-mail-sender-api/src/main/java/co/simplon/itp3/mailsender/repositories/EmailTemplateRepository.java
@@ -9,4 +9,6 @@ public interface EmailTemplateRepository
 	extends JpaRepository<EmailTemplate, Long> {
 
     EmailTemplateDto findProjectedById(Long id);
+
+    EmailTemplate getByTemplateIdentifier(String string);
 }
--- itp3-mail-sender-api/src/main/java/co/simplon/itp3/mailsender/services/CustomerServiceImpl.java
+++ itp3-mail-sender-api/src/main/java/co/simplon/itp3/mailsender/services/CustomerServiceImpl.java
@@ -2,6 +2,8 @@ package co.simplon.itp3.mailsender.services;
 
 import java.util.UUID;
 
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -9,8 +11,10 @@ import co.simplon.itp3.mailsender.config.ApiHelper;
 import co.simplon.itp3.mailsender.dtos.CreateCustomerDto;
 import co.simplon.itp3.mailsender.entities.ContactRole;
 import co.simplon.itp3.mailsender.entities.Customer;
+import co.simplon.itp3.mailsender.entities.EmailTemplate;
 import co.simplon.itp3.mailsender.repositories.ContactRoleRepository;
 import co.simplon.itp3.mailsender.repositories.CustomerRepository;
+import co.simplon.itp3.mailsender.repositories.EmailTemplateRepository;
 
 @Service
 @Transactional(readOnly = true)
@@ -23,12 +27,19 @@ public class CustomerServiceImpl
 
     private final ApiHelper apiHelper;
 
+    private final EmailTemplateRepository emailTemplates;
+    private final JavaMailSender javaMailSender;
+
     public CustomerServiceImpl(CustomerRepository customers,
 	    ContactRoleRepository contactRoles,
-	    ApiHelper apiHelper) {
+	    ApiHelper apiHelper,
+	    EmailTemplateRepository emailTemplates,
+	    JavaMailSender javaMailSender) {
 	this.customers = customers;
 	this.contactRoles = contactRoles;
 	this.apiHelper = apiHelper;
+	this.emailTemplates = emailTemplates;
+	this.javaMailSender = javaMailSender;
     }
 
     @Override
@@ -51,9 +62,32 @@ public class CustomerServiceImpl
 	String apiKey = UUID.randomUUID().toString();
 	String hash = apiHelper.encode(apiKey);
 	customer.setApiKey(hash);
+	sendApiKeyEmail(inputs, apiKey);
 	this.customers.save(customer);
 
     }
+
+    public void sendApiKeyEmail(CreateCustomerDto inputs,
+	    String ApiKey) {
+	EmailTemplate emailCustomer = new EmailTemplate();
+	emailCustomer = this.emailTemplates
+		.getByTemplateIdentifier("CUSTOMER");
+	System.out.println(emailCustomer);
+	try {
+	    SimpleMailMessage mailMessage = new SimpleMailMessage();
+	    mailMessage
+		    .setTo("juliette.madinier@gmail.com");
+	    mailMessage
+		    .setFrom("no-reply.dev@readresolve.io");
+	    mailMessage.setText(
+		    emailCustomer.getTemplateBody());
+	    mailMessage.setSubject(
+		    emailCustomer.getTemplateIdentifier());
+	    this.javaMailSender.send(mailMessage);
+	} catch (Exception e) {
+	    System.out.println(e);
+	}
+    };
 
     @Override
     public boolean emailValueExists(String email)
